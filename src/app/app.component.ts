af8de9f050d61764f82906b51e2b322ad9b3fedd
Matthew Waskin <matthew@waskin.com>
2023-09-11 19:03:28 -0400

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'assignment6-forms-td';
+  subTypes = ['Basic', 'Advanced', 'Pro'];
+  defaultSub = 'Advanced';
+  submitted = false;
+  user = {
+    email: '',
+    subType: '',
+    password: '',
+  };
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    this.submitted = true;
+    this.user.email = form.value.email;
+    this.user.subType = form.value.subType;
+    this.user.password = form.value.password;
+    form.reset();
+  }
 }
